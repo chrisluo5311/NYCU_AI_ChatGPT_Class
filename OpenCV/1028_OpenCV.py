dd6af63a5beb491e7913d95731d12ad5994bc62b
chrisluo5311 <chrislo5311@gmail.com>
2023-11-21 12:39:17 +0800

    update 0920 Pandas & numpy

--- OpenCV/1028_OpenCV.py
+++ OpenCV/1028_OpenCV.py
@@ -10,10 +10,11 @@ cv2.imshow("testImage", img)
 cv2.namedWindow("testImage_G")
 img_G = cv2.imread("MJ.png", cv2.IMREAD_GRAYSCALE)
 cv2.imshow("testImage_G", img_G)
-cv2.imwrite("./MJ_G.png", img_G)
-cv2.imwrite("./MJ_G.jpg", img_G)
-cv2.imwrite("./MJ_G_Low.jpg", img_G,
-            [int(cv2.IMWRITE_JPEG_QUALITY), 50])  # jpg格式特色: 可以壓縮圖片，但畫質差一點
+# cv2.imwrite("./MJ_G.png", img_G)
+# cv2.imwrite("./MJ_G.jpg", img_G)
+# cv2.imwrite("./MJ_G_Low.jpg", img_G,
+#             [int(cv2.IMWRITE_JPEG_QUALITY), 50])  # jpg格式特色: 可以壓縮圖片，但畫質差一點
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+#%%
